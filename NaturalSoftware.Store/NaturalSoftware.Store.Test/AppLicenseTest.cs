a65e06cb4be7b0981fa62158db31a0a439b7486e
kaorun <kaorun55@gmail.com>
2012-12-04 16:13:55 +0900

    LicenseInformation のラッパーを作成

---
+++ NaturalSoftware.Store/NaturalSoftware.Store.Test/AppLicenseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace NaturalSoftware.Store.Test
+{
+    [TestClass]
+    public class AppLicenseTest
+    {
+#if DEBUG
+        [TestMethod]
+        public void デバッグ時はシミュレーターでライセンス管理()
+        {
+            Assert.IsTrue( AppLicense.IsSimulator );
+        }
+#else
+        [TestMethod]
+        public void リリース時は実環境でライセンス管理()
+        {
+            Assert.IsFalse( AppLicense.IsSimulator );
+        }
+#endif
+    }
+}
